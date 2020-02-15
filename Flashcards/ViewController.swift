f0357988eb33c680c84bba8a6b1f1fda492c4ac9
Madison Borkovich <madisonborkovich@gmail.com>
2020-02-15 14:05:13 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Madison Borkovich on 2/15/20.
+//  Copyright © 2020 Madison Borkovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
